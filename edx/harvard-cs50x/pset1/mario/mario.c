db16ba096448ccab4cf4d432e2aa27ed025bef1b
e1630m <e1630m@gmail.com>
2021-08-27 13:45:51 +0000

    Update mario.c now using cs50.h

--- edx/harvard-cs50x/pset1/mario/mario.c
+++ edx/harvard-cs50x/pset1/mario/mario.c
@@ -1,35 +1,43 @@
+#include <cs50.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
-#define MAXTMP 100
 
 int main(void)
 {
     int h = -1;
     while (h < 1 || h > 8)
     {
-        printf("Height: ");
-        char tmp[MAXTMP];
-        scanf("%s", tmp);
+        string tmp = get_string("Height: ");
         for (int i = 0; i < strlen(tmp); i++)
         {
             if (!isdigit(tmp[i]))
+            {
                 break;
+            }
             if (i == strlen(tmp) - 1)
+            {
                 h = atoi(tmp);
+            }
         }
     }
 
     for (int i = 1; i <= h; i++)
     {
         for (int j = h - i; j > 0; j--)
+        {
             printf(" ");
+        }
         for (int j = 0; j < i; j++)
+        {
             printf("#");
+        }
         printf("  ");
         for (int j = 0; j < i; j++)
+        {
             printf("#");
+        }
         printf("\n");
     }
 }
