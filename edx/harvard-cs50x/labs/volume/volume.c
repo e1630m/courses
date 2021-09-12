bf564b6363d456050d754fa9f27a597dde3ead22
e1630m <e1630m@gmail.com>
2021-09-12 11:21:02 +0000

    Initial Commit cs50x/labs/volume

--- edx/harvard-cs50x/labs/volume/volume.c
+++ edx/harvard-cs50x/labs/volume/volume.c
@@ -34,8 +34,17 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+    uint8_t header[44];
+    fread(&header, 44, 1, input);
+    fwrite(&header, 44, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+    int16_t buffer;
+    while (fread(&buffer, sizeof(buffer), 1, input))
+    {
+        buffer *= factor;
+        fwrite(&buffer, sizeof(buffer), 1, output);
+    }
 
     // Close files
     fclose(input);
